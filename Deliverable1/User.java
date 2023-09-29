36f08c52f1d432a1df3bdaa21ef211961366f125
Aroha Upreti <uaroha@gmail.com>
2023-09-29 09:39:31 -0400

    add administrator, add setters in user

--- Deliverable1/User.java
+++ Deliverable1/User.java
@@ -1,27 +1,24 @@
 public class User{
-	
 	protected String firstName;
 	protected String lastName;
-	
 	protected String username;
 	protected String password;
 	protected String phoneNumber;
 	protected String address;
 	
 	public User(String firstName, 
-			String lastName, 
-			String username, 
-			String password, 
-			String phoneNumber, 
-			String address) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.username = username;
-		this.password = password;
-		this.phoneNumber = phoneNumber;
-		this.address = address;
-	}
-	
+				String lastName, 
+				String username, 
+				String password, 
+				String phoneNumber, 
+				String address) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.username = username;
+			this.password = password;
+			this.phoneNumber = phoneNumber;
+			this.address = address;
+		}
 	
 	protected String getFirstName() {
 		return this.firstName;
@@ -35,17 +32,31 @@ public class User{
 		return this.username;
 	}
 	
+	protected void setUsername(String newUsername) {
+		this.username = newUsername;
+	}
+	
 	protected String getPassword() {
 		return this.password;
+	}
+	
+	protected void setPassword(String newPassword) {
+		this.password = newPassword;
 	}
 	
 	protected String getPhoneNumber() {
 		return this.phoneNumber;
 	}
 	
-	public String getAddress() {
+	protected void setPhoneNumber(String newNumber) {
+		this.phoneNumber = newNumber;
+	}
+
+	protected String getAddress() {
 		return this.address;
 	}
 	
-	
+	protected void setAddress(String newAddress) {
+		this.address = newAddress;
+	}
 }

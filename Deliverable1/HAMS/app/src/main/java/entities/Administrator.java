90a32d8cbb5fea91630e560af54160764fd93ed6
Aroha Upreti <uaroha@gmail.com>
2023-10-06 11:48:25 -0400

    change directories

---
+++ Deliverable1/HAMS/app/src/main/java/entities/Administrator.java
@@ -0,0 +1,8 @@
+package entities;
+
+public class Administrator extends User{
+
+	public Administrator() {
+		super("", "", "admin", "admin", "", "");
+	}
+}
---
+++ Deliverable1/HAMS/app/src/main/java/entities/Doctor.java
@@ -0,0 +1,42 @@
+package entities;
+
+public class Doctor extends User{
+	
+	private int employeeNumber;
+	private String [] specialties;
+	
+	public Doctor(String firstName, 
+				String lastName, 
+				String username, 
+				String password, 
+				String phoneNumber, 
+				String address, 
+				int employeeNumber,
+				String [] specialties) {
+		super(firstName, lastName, username, password, phoneNumber, address);
+		this.employeeNumber = employeeNumber;
+		this.specialties = specialties;
+	}
+	
+	public int getEmployeeNumber() {
+		return this.employeeNumber;
+	}
+
+	public void setEmployeeNumber(int id) {
+		employeeNumber = id;
+	} 
+	
+	public String getSpecialties() {
+		String result = "";
+		for (int i = 0; i < specialties.length; i++) {
+			result += specialties[i] + ", ";
+		}
+		
+		return result.substring(0,specialties.length-2);
+	}
+	
+	public void setSpecialties(String [] s) {
+		specialties = s;
+	}
+	
+}

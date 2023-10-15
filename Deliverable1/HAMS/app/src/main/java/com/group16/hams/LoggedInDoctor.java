3a542fe7a7335c027e16fdf918528c272e51b210
mfawaz092 <mfawazph@gmail.com>
2023-10-15 00:02:19 -0400

    updated LoggedInDoctor and LoggedInPatient

--- Deliverable1/HAMS/app/src/main/java/com/group16/hams/LoggedInDoctor.java
+++ Deliverable1/HAMS/app/src/main/java/com/group16/hams/LoggedInDoctor.java
@@ -2,15 +2,41 @@ package com.group16.hams;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import entities.*;
 
 public class LoggedInDoctor extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private Button logoutButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.logged_in_doctor);
+
+        mAuth = FirebaseAuth.getInstance();
+        logoutButton = findViewById(R.id.logoutButton);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mAuth.signOut();
+                Intent intent = new Intent(LoggedInDoctor.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                Toast.makeText(LoggedInDoctor.this, "Logout Successful", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- Deliverable1/HAMS/app/src/main/java/com/group16/hams/LoggedInPatient.java
+++ Deliverable1/HAMS/app/src/main/java/com/group16/hams/LoggedInPatient.java
@@ -2,15 +2,39 @@ package com.group16.hams;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import entities.*;
 
 public class LoggedInPatient extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private Button logoutButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.logged_in_patient);
+
+        mAuth = FirebaseAuth.getInstance();
+        logoutButton = findViewById(R.id.logoutButton);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mAuth.signOut();
+                Intent intent = new Intent(LoggedInPatient.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                Toast.makeText(LoggedInPatient.this, "Logout Successful", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

dda9f31bffad53c1607750ddd0bac532bdb6b492
Gabriel Sher <gsher047@uottawa.ca>
2023-11-06 12:16:46 -0500

    Created appointment constructor

--- Deliverable1/HAMS/app/src/main/java/entities/Appointment.java
+++ Deliverable1/HAMS/app/src/main/java/entities/Appointment.java
@@ -3,9 +3,25 @@ package entities;
 import java.util.Date;
 
 public class Appointment {
-    String dateAndTimeString;
+    Patient appointmentPatient;
 
-    Date dateAndTime;
+    String appointmentPatientEmail;
+    String startDateAndTimeString;
+    String endDateAndTimeString;
+
+    Date startDateAndTime;
+    Date endDateAndTime;
+
+    boolean upcoming = true;
 
 
+    public Appointment(String appointmentPatientEmail, String startDateAndTimeString, String endDateAndTimeString) {
+        this.appointmentPatientEmail = appointmentPatientEmail;
+        this.startDateAndTimeString = startDateAndTimeString;
+        this.endDateAndTimeString = endDateAndTimeString;
+
+        //ADD A METHOD HERE THAT GETS THE PATIENT FROM A METHOD IN DATABASE THAT FINDS THE PATIENT USING THE EMAIL
+
+        //CONVERT DATE STRINGS TO ACTUAL DATES
+    }
 }

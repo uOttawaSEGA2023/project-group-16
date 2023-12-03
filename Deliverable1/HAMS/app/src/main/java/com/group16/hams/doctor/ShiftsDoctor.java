a6dbc6e1a836157fa7a766725529ed0fcd441614
kostyarnouk <kostyarnouk@gmail.com>
2023-12-02 20:10:22 -0500

    Doctor conditions added inside the ShiftsDoctor class

--- Deliverable1/HAMS/app/src/main/java/com/group16/hams/doctor/ShiftsDoctor.java
+++ Deliverable1/HAMS/app/src/main/java/com/group16/hams/doctor/ShiftsDoctor.java
@@ -18,6 +18,7 @@ import com.group16.hams.ShiftClicked;
 
 import java.util.ArrayList;
 
+import entities.Appointment;
 import entities.Doctor;
 import entities.Shift;
 
@@ -65,10 +66,12 @@ public class ShiftsDoctor extends AppCompatActivity implements RecyclerViewInter
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void setUpAppointmentHolders() {
         ArrayList<Shift> shifts = ((Doctor) Database.currentUser).getShifts();
+        ArrayList<Appointment> appointments = ((Doctor) Database.currentUser).getAppointments();
         System.out.println(shifts.size());
         System.out.println("Shift: " + shifts);
         Shift curShift;
         ArrayList<Shift> shiftsToDelete = new ArrayList<>();
+
         for (int i = 0; i < shifts.size(); i++) {
             curShift = shifts.get(i);
 
@@ -77,8 +80,15 @@ public class ShiftsDoctor extends AppCompatActivity implements RecyclerViewInter
                         curShift.getStartTime(), curShift.getEndTime(), 1, curShift));
             }
             else{
-                Shift delShift = new Shift(curShift.getDate(), curShift.getStartTime(), curShift.getEndTime());
-                shiftsToDelete.add(delShift);
+                for (Appointment appointment : appointments) {
+                    if (isOverlapping(curShift, appointment)) {
+                        // alert user of overlap and inability to remove appointment
+                    } else {
+                        Shift delShift = new Shift(curShift.getDate(), curShift.getStartTime(), curShift.getEndTime());
+                        shiftsToDelete.add(delShift);
+                    }
+
+                }
             }
         }
         for (Shift delShift : shiftsToDelete) {
@@ -90,6 +100,50 @@ public class ShiftsDoctor extends AppCompatActivity implements RecyclerViewInter
                 }
             }, 1000);
         }
+    }
+
+    private boolean isOverlapping (Shift shift, Appointment appointment) {
+        String shiftDate = shift.getDate();
+        String shiftStart = shift.getStartTime();
+        String shiftEnd = shift.getEndTime();
+
+        String[] appointmentDateTime = appointment.getStartDateAndTimeString().split(" ");
+        String appointmentDate = appointmentDateTime[0];
+        String appointmentStartTime = appointmentDateTime[1];
+        String appointmentEndTime = addMinutes(appointmentStartTime, 30);
+
+        int intShiftStart = Integer.parseInt(shiftStart);
+        int intShiftEnd = Integer.parseInt(shiftEnd);
+        int intApptStart = Integer.parseInt(appointmentStartTime);
+        int intApptEnd = Integer.parseInt(appointmentEndTime);
+
+        if (appointmentDate == shiftDate) {
+            if (intShiftStart < intApptStart && intApptEnd < intShiftEnd) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    private String addMinutes(String time, Integer minutesToAdd) {
+        String[] hourMin = time.split(":");
+        int hour = Integer.parseInt(hourMin[0]);
+        int min = Integer.parseInt(hourMin[1]);
+
+        min += minutesToAdd;
+
+        if (min >= 60) {
+            hour += 1;
+            min -= 60;
+        }
+
+        if (hour >= 24) {
+            hour -= 24;
+        }
+
+        return String.format("%02d:%02d", hour, min);
+
     }
     @Override
     public void onItemClick(int type, int position) {
@@ -115,3 +169,4 @@ public class ShiftsDoctor extends AppCompatActivity implements RecyclerViewInter
         startActivity(intent);
     }
 }
+
